1776e799a5cf8d48e63d8196012ea8ea3e337436
ryan <ryan@build-platform.perl.home.simcop2387.info>
2022-06-06 11:44:51 -0400

    missing patches

--- build.sh
+++ build.sh
@@ -4,10 +4,12 @@ set -euo pipefail
 
 for build in 5*; do
 	TAG=simcop2387/perl:$(echo $build | perl -pE 's/,/-/g')
+	LOCAL_TAG=registry.docker.home.simcop2387.info:443/simcop2387/perl:$(echo $build | perl -pE 's/,/-/g')
 	echo building $TAG...
         ( cd $build;
-        docker build -t $TAG .
-        docker push $TAG
+        docker buildx build --platform=linux/amd64,linux/arm64 --progress=simple -t $TAG -t $LOCAL_TAG . 2>&1 && \
+        docker push $TAG && \
+        docker push $LOCAL_TAG
         ) | ts "$TAG [%H:%M:%S]" | tee build.$TAG.log || echo "  Failed to build $TAG"
 done
 
