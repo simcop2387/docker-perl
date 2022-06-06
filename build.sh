938eae887cc281d4640379b36ceec1410e0f25de
ryan <ryan@build-platform.perl.home.simcop2387.info>
2022-06-06 12:25:23 -0400

    disable tests, qemu emulated builds are failing a few of the tests even though they should pass otherwise.  will rebuild with tests once i get something scripted up again for the arm system to pull the x86 version and do manifest manipulation

--- build.sh
+++ build.sh
@@ -5,6 +5,11 @@ set -euo pipefail
 for build in 5*; do
 	TAG=simcop2387/perl:$(echo $build | perl -pE 's/,/-/g')
 	LOCAL_TAG=registry.docker.home.simcop2387.info:443/simcop2387/perl:$(echo $build | perl -pE 's/,/-/g')
+	PLATFORMS=linux/amd64,linux/arm64
+	if [[ $build == *"quadmath"* ]]; then
+		# exclude arm64 from quadmath builds since it doesn't apply
+		PLATFORMS=linux/amd64
+	fi
 	echo building $TAG...
         ( cd $build;
         docker buildx build --platform=linux/amd64,linux/arm64 --progress=simple -t $TAG -t $LOCAL_TAG . 2>&1 && \
